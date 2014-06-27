c5e0f177b0bf084e73231e408f19702b7832c2c0
icymorn <icymorn@gmail.com>
2014-06-27 17:50:56 +0800

    scss suported.
    customize import css and js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,37 @@
+'use strict';
+
 module.exports = function (grunt) {
-	grunt.initConfig({
-    jshint: {
-      src: ['Gruntfile.js', 'app.js', 'settings.js', 'routes/*.js'],
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        node: true,
-        globals: {
-          require: true,
-          define: true,
-          requirejs: true,
-          describe: true,
-          expect: true,
-          it: true
+    grunt.initConfig({
+        'jshint': {
+            src: ['Gruntfile.js', 'app.js', 'settings.js', 'routes/*.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                node: true,
+                globals: {
+                    require: true,
+                    define: true,
+                    requirejs: true,
+                    describe: true,
+                    expect: true,
+                    it: true
+                }
+          }
         }
-      }
-    }
-  });
+    });
 
-  // Load JSHint task
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+    // Load JSHint task
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  // Default task.
-  grunt.registerTask('default', 'jshint');
-
+    // Default task.
+    grunt.registerTask('default', 'jshint');
 };
--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-﻿var express = require("express");
-var routes = require("./routes");
-var http = require("http");
-var path = require('path');
-var MongoStore = require("connect-mongo")(express);
-var settings = require("./settings");
-var flash = require('connect-flash');
+﻿'use strict';
+
+var express    = require('express');
+var routes     = require('./routes');
+var http       = require('http');
+var path       = require('path');
+var MongoStore = require('connect-mongo')(express);
+var settings   = require('./settings');
+var flash      = require('connect-flash');
+var sass       = require('node-sass');
 
 
 var app = express();
@@ -13,6 +16,8 @@ app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
+
+
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser({keepExtensions: true, uploadDir: './public/images'}));
@@ -31,14 +36,45 @@ app.configure(function () {
             db: settings.db
         })
     }));
+
+    app.use(sass.middleware({
+        src: __dirname + '/precompile',
+        dest: __dirname+ '/public',
+        debug: true,
+        outputStyle: 'compressed'//'expanded'
+    }));
+    
     app.use(app.router);
 
-    if ('development' == app.get("env")) {
+    if ('development' == app.get('env')) {
         app.use(express.errorHandler());
     }
 });
 
-app.locals.logo = "XMORN";
+app.locals({
+    logo: 'XMORN',
+    script: [],
+    css: [],
+    getScriptTags: function () {
+        if (this.script.length >0 ) {
+            return this.script.map(function (filename) {
+                return '<script src=\"' + filename + '\"></script>';
+            }).join('\n');
+        }else{
+            return '';
+        }
+
+    },
+    getStyleTags: function () {
+        if (this.css.length >0 ) {
+            return this.css.map(function (filename) {
+                return '<link rel=\"stylesheet\" href=\"' + filename + '\"/>';
+            }).join('\n');
+        }else{
+            return '';
+        }
+    }
+});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port' + app.get('port'));
