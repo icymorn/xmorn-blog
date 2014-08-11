ba828195bd5915b3ccf979f83069906af5a5c56e
moe <icymorn@gmail.com>
2014-08-12 01:44:20 +0800

    blog init

--- app.js
+++ app.js
@@ -1,14 +1,71 @@
-/**
- * Created by moe on 8/10/14.
- */
-
-// Module dependencies.
 var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var sass = require('node-sass');
+//var compass = require('node-compass');
 
 var app = express();
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+app.use(sass.middleware({
+    src: __dirname + '/public',
+    dest: __dirname+ '/public',
+    debug: true,
+    outputStyle: 'expanded'
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(compass({mode: 'expanded'}));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.listen(3000);
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
